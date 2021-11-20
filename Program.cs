3551aa5b9c7c11ab7981caaed53dbcd7519db547
Paul Krisadaolarn <pauly.k@gmail.com>
2021-11-20 11:00:14 +0700

    Spelling

--- Program.cs
+++ Program.cs
@@ -3,11 +3,8 @@ global using System;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-
-
 app.MapGet("/", () => "RMA CML Test Firewall endpoint");
-app.MapGet("/todo", () => new Todo(Title: "Update Firewall", Body: "Update IPS firewall to allo CML to communicate with Azure backend"));
-
+app.MapGet("/todo", () => new Todo(Title: "Update Firewall", Body: "Update ISP firewall to allow CML to communicate with Azure backend"));
 
 
 app.Run();
