4558478b3b9c8db9b8f8b62f31d2b9332b8dcbcd
Paul Krisadaolarn <pauly.k@gmail.com>
2021-11-20 10:54:31 +0700

    Add todo endpoint

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
+global using System;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+
+
 app.MapGet("/", () => "RMA CML Test Firewall endpoint");
+app.MapGet("/todo", () => new Todo(Title: "Update Firewall", Body: "Update IPS firewall to allo CML to communicate with Azure backend"));
+
+
 
 app.Run();
---
+++ Util.cs
@@ -0,0 +1,1 @@
+public record Todo(string Title, string Body);
